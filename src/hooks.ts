7419b0ed2d19908d77ededc63aefc2b9ae1b4a75
Anwesh43 <anweshthecool0@gmail.com>
2021-12-05 22:51:45 +0530

    adding useAnimatedScale hook

---
+++ src/hooks.ts
@@ -0,0 +1,28 @@
+import {useState, useEffect} from 'react'
+import { createBrotliCompress } from 'zlib'
+
+const delay : number = 20 
+const scGap : number = 0.01 
+
+export const useAnimatedScale = () => {
+    const [scale, setScale] = useState(0)
+    const [animated, setAnimated] = useState(false)
+    return {
+        scale, 
+        start() {
+            if (!animated) {
+                setAnimated(true)
+                const interval = setInterval(() => {
+                    setScale((prev : number) => {
+                        if (prev > 1) {
+                            setAnimated(false)
+                            clearInterval(interval)
+                            return 0
+                        }
+                        return prev + scGap 
+                    })
+                }, delay)
+            }
+        }
+    }
+}
